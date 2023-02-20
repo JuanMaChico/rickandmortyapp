76a1e484d924d273177bd47def007bee45907440
JuanMaChico <juanmanuelchico@hotmail.com>
2023-02-20 13:54:28 -0300

    Ultimos cambios en la app con CRA

--- src/Context/Context.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Context = React.createContext({
-	characterOne: false,
-	characterTwo: false,
-});
-
-export default Context;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Views/App';
-import Context from './Context/Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-	<Context.Provider
-		value={{
-			characterOne: false,
-			characterTwo: false,
-		}}
-	>
-		<App />
-	</Context.Provider>,
-);
+root.render(<App />);
