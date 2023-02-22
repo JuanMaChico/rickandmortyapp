da87a8a6a601d61d32f9834bdc1f082bf435a9c9
JuanMaChico <juanmanuelchico@hotmail.com>
2023-02-22 10:28:23 -0300

    Comments in viteconfig

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,10 @@
  *
  */
 
+//Dependencias
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	test: {
